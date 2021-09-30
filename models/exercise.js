8eda9dccd8842716cbb18ea0a7f3160d8fc5d0ec
Sean Russell <emailseanrussell@gmail.com>
2021-09-30 13:35:46 -0700

    fixed broken route for duration and graph

--- models/exercise.js
+++ models/exercise.js
@@ -1,16 +1,16 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
 
-const schema = new Schema({
-type: String,
-name: String,
-duration: Number,
-weight: Number,
-reps: Number,
-sets: Number,
-});
+// const schema = new Schema({
+// type: String,
+// name: String,
+// duration: Number,
+// weight: Number,
+// reps: Number,
+// sets: Number,
+// });
 
-const Excercise = mongoose.model("Excercise", schema);
+// const Excercise = mongoose.model("Excercise", schema);
 
-module.exports = Excercise;
+// module.exports = Excercise;
--- models/workout.js
+++ models/workout.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const workoutSchema = new Schema({
+
+const schemaWorkouts = new Schema({
     day: { type: Date,
         default: () => new Date() },
     exercises: [
@@ -33,5 +34,11 @@ const workoutSchema = new Schema({
 }
 );
 
-const Workout = mongoose.model('Workout', workoutSchema);
+schemaWorkouts.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const Workout = mongoose.model('Workout', schemaWorkouts);
 module.exports = Workout;
